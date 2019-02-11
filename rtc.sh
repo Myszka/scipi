4e302a44dec376d81c41884d4fdbee2dfc506a2a
MiCh <mich@myszka.waw.pl>
2019-02-11 17:41:03 +0100

    RTC on i2c-0 and i2c-1

--- rtc.sh
+++ rtc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sleep 15
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-0/new_device
+echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 hwclock -s -f /dev/rtc1
 unlink /dev/rtc
 ln -s /dev/rtc1 /dev/rtc
